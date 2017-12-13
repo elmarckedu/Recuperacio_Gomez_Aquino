5c0a748822b57ece14df43b9cd0128caf54cd11c
elmarckedu <tuzzo2306@outlook.com>
2017-12-12 20:06:34 -0600

    suma

--- hola_mundo.cpp
+++ hola_mundo.cpp
@@ -3,8 +3,15 @@
 using namespace std;
 
 int main() {
-
+    int a,b,r;
     cout << "Hola Mundo" << endl;
+    cout<< "ingresa un numero"<<endl;
+    cin>> a;
+    cout<< "ingrese otro numero"<<endl;
+    cin>> b;
+    r=a+b;
+    cout<< " la suma es = "<< r<<endl;
+    
 
     return 0;
 
