05d753e56e1fe5547f1903176bf2337d155a68d9
elmarckedu <tuzzo2306@outlook.com>
2017-12-12 20:07:34 -0600

    factorial

--- hola_mundo.cpp
+++ hola_mundo.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    int a,b,r;
+    int a,b,r,c,f;
     cout << "Hola Mundo" << endl;
     cout<< "ingresa un numero"<<endl;
     cin>> a;
@@ -11,6 +11,16 @@ int main() {
     cin>> b;
     r=a+b;
     cout<< " la suma es = "<< r<<endl;
+      cout<< "numero a sacar factorial"<<endl;
+   cin>>c;
+    if(c<0) f =0;
+   else if(c==0) f=1;
+   else{
+      for (int i = 1; i <= c; i++){
+         f = f*i;
+      }
+   }
+   cout<<"Factorial de "<<c<<" es "<<f;
     
 
     return 0;
